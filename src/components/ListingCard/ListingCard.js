4797c150d472b1af3b8bd7598cd6c6ce82f520e4
u-f-0 <carl@u-f-o.se>
2024-01-21 00:24:32 +0100

    Fix listing type

--- src/components/ListingCard/ListingCard.js
+++ src/components/ListingCard/ListingCard.js
@@ -120,7 +120,7 @@ export const ListingCardComponent = props => {
           sizes={renderSizes}
         />
           <div className={css.listingTypeWrapper}>
-            {currentListing.attributes.publicData.listingType === "sell-products"
+            {publicData.listingType === "sell-products"
               ? <div className={css.listingType}>Selling</div>
               : <div className={css.listingType}>Aquiring</div>
             }
